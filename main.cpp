513dbf7d720df7d916cfc9dcaab442dd55d9e3df
R7600\watsk <watskmt@gmail.com>
2026-05-14 11:11:26 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+﻿#pragma warning(push)
+#pragma warning(disable: 4828 4010)
+#include "DxLib.h"
+#pragma warning(pop)
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    ChangeWindowMode(TRUE);
+    SetGraphMode(640, 480, 32);
+    SetMainWindowText(L"DXLib Project");
+
+    if (DxLib_Init() == -1)
+    {
+        return -1;
+    }
+
+    SetDrawScreen(DX_SCREEN_BACK);
+
+    while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+    {
+        ClearDrawScreen();
+
+        DrawString(10, 10, L"Hello, DXLib!", GetColor(255, 255, 255));
+
+        ScreenFlip();
+    }
+
+    DxLib_End();
+    return 0;
+}
