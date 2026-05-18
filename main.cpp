6862f78472fc6dcf74a2f0092f02c43653e07db4
LAPTOP-OBQO5216\PC-YUUTO <zen-yuuto@amami-joho.ac.jp>
2026-05-18 09:19:18 +0900

    盤面の描画

--- main.cpp
+++ main.cpp
@@ -3,10 +3,24 @@
 #include "DxLib.h"
 #pragma warning(pop)
 
+#define BOARD_SIZE 8
+#define CELL_SIZE 80
+#define STONE_SIZE 30
+#define WINDOW_WIDTH  (BOARD_SIZE * CELL_SIZE)
+#define WINDOW_HEIGHT (BOARD_SIZE * CELL_SIZE)
+#define EMPTY 0
+#define BLACK 1
+#define WHITE 2
+
+int board[BOARD_SIZE][BOARD_SIZE];
+
+void InitBoard(void);
+void drawBoard(void);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     ChangeWindowMode(TRUE);
-    SetGraphMode(640, 480, 32);
+    SetGraphMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32);
     SetMainWindowText(L"DXLib Project");
 
     if (DxLib_Init() == -1)
@@ -16,15 +30,43 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     SetDrawScreen(DX_SCREEN_BACK);
 
+    InitBoard();
+
     while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
     {
         ClearDrawScreen();
 
-        DrawString(10, 10, L"Hello, DXLib!", GetColor(255, 255, 255));
+        drawBoard();
 
         ScreenFlip();
     }
 
     DxLib_End();
     return 0;
+}
+
+void InitBoard(void)
+{
+    for (int y = 0; y < BOARD_SIZE; y++)
+    {
+        for (int x = 0; x < BOARD_SIZE; x++)
+        {
+            board[y][x] = EMPTY;
+        }
+    }
+}
+
+void drawBoard(void)
+{
+    DrawBox(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, GetColor(0, 150, 0), TRUE);
+
+    for (int y = 0; y <= BOARD_SIZE; y++)
+    {
+        DrawLine(0, y * CELL_SIZE, WINDOW_WIDTH, y * CELL_SIZE, GetColor(0, 0, 0));
+    }
+
+    for (int x = 0; x <= BOARD_SIZE; x++)
+    {
+        DrawLine(x * CELL_SIZE, 0, x * CELL_SIZE, WINDOW_HEIGHT, GetColor(0, 0, 0));
+    }
 }
